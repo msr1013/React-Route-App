89bd875c97d2eea5c5d09df3865eb1271d5145b5
Manish Rawal <rawalsinghmanish@gmail.com>
2021-06-04 11:20:10 +0545

    adding-routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Home from "./components/home";
+import Posts from "./components/posts";
+import "./App.css";
+import { Route } from "react-router-dom";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="content">
+          <Route path="/products" component={Products} />
+          <Route path="/posts" component={Posts} />
+          {/* <Route path="/admin" component={Dashboard} /> */}
+          <Route path="/" component={Home} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
