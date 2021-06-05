08bd0fbacc30177ddce68a9de857f654c8c54ec2
Manish Rawal <rawalsinghmanish@gmail.com>
2021-06-05 08:47:26 +0545

    passsing-custom-route-props-to-products-component-and-using-route-parameter-in-productDetails-component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Home from "./components/home";
 import Posts from "./components/posts";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Component } from "react";
 
 class App extends Component {
@@ -12,10 +14,16 @@ class App extends Component {
       <div>
         <NavBar />
         <div className="content">
-          <Route path="/products" component={Products} />
-          <Route path="/posts" component={Posts} />
-          {/* <Route path="/admin" component={Dashboard} /> */}
-          <Route path="/" component={Home} />
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route
+              path="/products"
+              render={(props) => <Products sortBy="newest" {...props} />}
+            />
+            <Route path="/posts/:year/:month" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
         </div>
       </div>
     );
