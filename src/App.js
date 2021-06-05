e84556b0c661913a202555ea8d1832cf22d5bf48
Manish Rawal <rawalsinghmanish@gmail.com>
2021-06-05 10:37:18 +0545

    parsing-using-queryString-for-location-props-and-implement-redirects

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Home from "./components/home";
 import Posts from "./components/posts";
+import NotFound from "./components/notFound";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { Component } from "react";
 
 class App extends Component {
@@ -20,9 +21,12 @@ class App extends Component {
               path="/products"
               render={(props) => <Products sortBy="newest" {...props} />}
             />
+            <Route path="/not-found" component={NotFound} />
             <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Redirect from="/messages" to="/posts" />
+            <Route path="/" exact component={Home} />
+            <Redirect to="not-found" />
           </Switch>
         </div>
       </div>
